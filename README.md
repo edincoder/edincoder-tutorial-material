6c96493858a5bfd09baf01760ad39f62fea006d0
Andrew Beveridge <andrew@beveridge.uk>
2018-01-26 03:58:05 +0000

    Finished help text for level 3, updated README. Now bedtime.

--- README.md
+++ README.md
@@ -1,26 +1,36 @@
 # Edincoder: Tutorial Materials
 
 ## Level 1: [Demo](http://one.edincoder.tk) / [Codepen](https://codepen.io/beveradb/pen/NXZQzv)
-Screenshot:
+- Outcome: Simple text/image based "About Me" page, with default browser styling
+- Learning: Basic HTML tags / elements
+- Screenshot:
 ![Level 1 Screenshot](http://one.edincoder.tk/screenshots/level-1-step-6.png "Level 1 Screenshot")
 
 
 ## Level 2: [Demo](http://two.edincoder.tk) / [Codepen](https://codepen.io/beveradb/pen/)
-Screenshot:
+- Outcome: Simple "About Me" page, hopefully more aesthetically pleasing
+- Learning: Introductory CSS rules and properties
+- Screenshot:
 ![Level 2 Screenshot](http://two.edincoder.tk/screenshots/level-2-step-8.png "Level 2 Screenshot")
 
 
 ## Level 3: [Demo](http://three.edincoder.tk) / [Codepen](https://codepen.io/beveradb/pen/)
-Screenshot:
-![Level 3 Screenshot](http://three.edincoder.tk/screenshots/level-3-step-.png "Level 3 Screenshot")
+- Outcome: Further styling of "About Me" page, with small amount of interactivity, button which launches email client
+- Learning: Further CSS, introduction to JavaScript
+- Screenshot:
+![Level 3 Screenshot](http://three.edincoder.tk/screenshots/level-3-step-9.png "Level 3 Screenshot")
 
 
 ## Level 4: [Demo](http://four.edincoder.tk) / [Codepen](https://codepen.io/beveradb/pen/)
-Screenshot:
+- Outcome: 
+- Learning: 
+- Screenshot:
 ![Level 4 Screenshot](http://four.edincoder.tk/screenshots/level-4-step-.png "Level 4 Screenshot")
 
 
 ## Level 5: [Demo](http://five.edincoder.tk) / [Codepen](https://codepen.io/beveradb/pen/)
-Screenshot:
+- Outcome: 
+- Learning: 
+- Screenshot:
 ![Level 5 Screenshot](http://five.edincoder.tk/screenshots/level-5-step-.png "Level 5 Screenshot")
 
--- three/main.js
+++ three/main.js
@@ -1,6 +1,20 @@
 
-/* Edincoder Level 3 -> JavaScript Step 9: Make the "Hire Me" button trigger an email message to you when clicked
-*  To do this, */
+/* Edincoder Level 3 -> JavaScript Step 9: Make the "Hire Me" button trigger an email message to you when clicked.
+*  To do this:
+*   Create a function and assign it to a helpfully-named variable, e.g. "var setupHireMeButtonClick = function() {}"
+*   Inside that function, use document.getElementById to get your Hire Me button element, and assign this to a variable, e.g. "var hiremeButton = document.getElementById('your-button-id')"
+*   Call addEventListener on the hiremeButton to add a "click" event listener to that button - the listener will be another function, which will get called whenever the button is clicked.
+*     e.g. hiremeButton.addEventListener("click", function(e) {}, false);
+*   Inside your event listener function, create an anchor / link element (<a> tag), and assign this to a variable - e.g. var hiremeMailtoLink = document.createElement('a');
+*   Set the "href" property of the link element to "mailto:YOUREMAIL", replacing YOUREMAIL with your email address
+*   Set the "target" property of the link element t "_blank" - this tells your web browser to open the link in a new tab/window
+*   Finally, call click() on the link element - this will trigger the browser functionality for clicking "mailto:" links, which should trigger an email client to open a compose email window!
+*
+*   Now we've got a function (e.g. "setupHireMeButtonClick") which /sets up/ the click event handler on the button, we still need to call that function!
+*   However, we need to wait until the webpage has loaded before trying to do this, as otherwise your web browser might not have loaded all of the HTML tags yet.
+*   To do this, we can add an event listener on the document itself, listening for the event "DOMContentLoaded" which means the whole HTML page has been loaded and processed by the browser.
+*     e.g. document.addEventListener('DOMContentLoaded', setupHireMeButtonClick, false);
+*/
 var setupHireMeButtonClick = function() {
     var hiremeButton = document.getElementById('hireme');
 
